829a9aba248a94580a2b89f884c75a1fff2fa37d
ppareplum <144793090+ppareplum@users.noreply.github.com>
2023-09-12 18:59:55 +0700

    Update water3.py

--- task-1/water3.py
+++ task-1/water3.py
@@ -1,9 +1,36 @@
 def initial_state():
-    return (0, 0, 0)
+    return (8, 0, 0)
 
 def is_goal(s):
-    return True
+    return s[0] == 4 and s[1] == 4
 
 def successors(s):
+    actions = []
     x, y, z = s
-    return []
+
+    if x > 0 and y < 5:
+        amount = min(x, 5 - y)
+        actions.append(((x - amount, y + amount, z), amount))
+
+    if x > 0 and z < 3:
+        amount = min(x, 3 - z)
+        actions.append(((x - amount, y, z + amount), amount))
+
+    if y > 0 and x < 8:
+        amount = min(y, 8 - x)
+        actions.append(((x + amount, y - amount, z), amount))
+
+    if y > 0 and z < 3:
+        amount = min(y, 3 - z)
+        actions.append(((x, y - amount, z + amount), amount))
+
+    if z > 0 and x < 8:
+        amount = min(z, 8 - x)
+        actions.append(((x + amount, y, z - amount), amount))
+
+    if z > 0 and y < 5:
+        amount = min(z, 5 - y)
+        actions.append(((x, y + amount, z - amount), amount))
+
+    return actions  
+
